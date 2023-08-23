0810e35a863def20194d0b20807fd3ed0b043782
César Girón <ipog71@gmail.com>
2023-08-23 16:26:38 -0600

    subidos documentos de php

--- Act 2/index.php
+++ Act 2/index.php
@@ -1,6 +1,33 @@
-<?php
-$a = 2;
-$b = 4;
-echo $a + $b;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica PHP con apache</title>
+    <style>
+        label {
+            display: block;
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Contacto</h1>
+    <form method="get">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre">
 
-?>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+
+        <label for="mensaje">Mensaje</label>
+        <textarea name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+    echo "<h1>Prueba</h1>";
+    ?>
+</body>
+</html>
