b38845388d9fa4e14b413693a3744f16e2306b59
César Girón <ipog71@gmail.com>
2023-08-24 10:41:28 -0600

    inserción de datos terminada

---
+++ Act 2/config.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = '172.22.250.138';
+$databaseUsername = 'gambled';
+$databasePassword = '0806';
+$databaseName = 'prueba';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
--- Act 2/index.php
+++ Act 2/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Practica PHP con apache</title>
+    <title>Practica remota PHP 6</title>
     <style>
         label {
             display: block;
@@ -14,25 +14,47 @@
 <body>
     <h1>Contacto</h1>
     <form method="post">
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre">
+        <label for="ID">ID</label>
+        <input type="text" name="ID" id="ID">
 
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+        <label for="name">name</label>
+        <input type="text" name="name" id="name">
 
-        <label for="mensaje">Mensaje</label>
-        <textarea name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
-        <br>
         <input type="submit" value="Enviar">
     </form>
 
     <?php
-    echo "<h1>Prueba</h1>";
-    if (isset($_POST['nombre'])) { //Para validar que el formulario se haya enviado, y no es una variable vacía
-        $nombre = $_POST['nombre'];
-        echo "<p>Hola " . $nombre . " gracias por tu mensaje" . "</p>";
-        echo "<h2>Email: " . $_POST['email'] . "</h2>";
-        echo "<h2>Mensaje: " . $_POST['mensaje'] . "</h2>";
+    // REPORTING DE ERRORES
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    // Include the database connection file
+    include_once("config.php");
+    if (isset($_POST['name'])) { //Para validar que el formulario se haya enviado, y no es una variable vacía
+        $id = $_POST['ID'];
+        $name = $_POST['name'];
+
+        echo "<h1>$name agregado a la base de datos con la id $id</h1>";
+        
+        //Insertar usuario en base de datos
+        $stmt = $mysqli->prepare("INSERT INTO employees (id,name) VALUES($id,'$name')");
+        $stmt->execute();
+
+        //Mostrar usuarios de base de datos en una tabla
+        $result = $mysqli->query("SELECT * FROM employees");
+        echo "<h1>Usuarios en base de datos</h1>";
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Nombre</th>";
+        echo "</tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['name']."</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
     }
     ?>
 </body>
