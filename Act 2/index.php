4e87cb331724998f0e90ea476522db44926c1f8a
César Girón <ipog71@gmail.com>
2023-08-29 10:51:36 -0600

    xd

--- Act 2/index.php
+++ Act 2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
         }
     </style>
 </head>
+
 <body>
     <h1>Contacto</h1>
     <form method="post">
@@ -28,34 +30,38 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+
     // Include the database connection file
     include_once("config.php");
+    
+    //Mostrar usuarios de base de datos en una tabla
+    $result = $mysqli->query("SELECT * FROM employees");
+    echo "<h1>Usuarios en base de datos</h1>";
+    echo "<table border='1'>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>Nombre</th>";
+    echo "</tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
     if (isset($_POST['name'])) { //Para validar que el formulario se haya enviado, y no es una variable vacía
         $id = $_POST['ID'];
         $name = $_POST['name'];
 
         echo "<h1>$name agregado a la base de datos con la id $id</h1>";
-        
+
         //Insertar usuario en base de datos
         $stmt = $mysqli->prepare("INSERT INTO employees (id,name) VALUES($id,'$name')");
         $stmt->execute();
 
-        //Mostrar usuarios de base de datos en una tabla
-        $result = $mysqli->query("SELECT * FROM employees");
-        echo "<h1>Usuarios en base de datos</h1>";
-        echo "<table border='1'>";
-        echo "<tr>";
-        echo "<th>ID</th>";
-        echo "<th>Nombre</th>";
-        echo "</tr>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>".$row['id']."</td>";
-            echo "<td>".$row['name']."</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
+
     }
     ?>
 </body>
+
 </html>
